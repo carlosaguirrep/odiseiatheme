462e78344f7e9909a9eaf7d88285503b2ec4471d
Carlos Alberto Aguirre Pacheco <carlos@ts-connect.de>
2025-02-09 14:34:08 -0600

    OdiseIaTheme 0.1

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+function mi_tema_fse_enqueue_styles() {
+    wp_enqueue_style('mi-tema-fse-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'mi_tema_fse_enqueue_styles');
+
+// Soporte para características básicas
+add_theme_support('post-thumbnails');
+add_theme_support('responsive-embeds');
+add_theme_support('editor-styles');
